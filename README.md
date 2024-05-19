8049f56794a50a84959868d2f8bbf21340e01689
nomodeset <nomodeset@qq.com>
2024-05-19 16:59:03 +0800

    Certain documentation(?)

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Visual-MQAR
 Understand and test multi-modal language model architectures on Visual-MQAR task.
+
+## Introduction
+
+This repository contains the code for the Visual-MQAR task. The task is a benchmark for evaluating the performance of multi-modal language models on visual inputs.
+
+The main task is such: given a text prompt and a set of images, the model should generate a natural language response that is consistent with the text prompt and the images. The model should be able to extract relevant information from both the text and the images and generate a coherent and fluent response.
+
+## Dataset and Preprocessing
+
+You can build up the dataset by following the steps below:
+
+```bash
+cd tests
+python test_dataset_generation.py
+```
+
+This will generate the dataset in the `tests/data` directory.
+
+It is based on the openwikitext dataset, which is a large-scale multilingual dataset for text generation. We use the English version of the dataset for this task.
+
+
+## Training
+
+To train a multi-modal language model on the Visual-MQAR task, follow the steps below:
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/xforcevesa/Visual-MQAR.git
+```
+
+2. Install the required packages:
+
+```bash
+pip install -r requirements.txt
+```
+
+3. Test the training script:
+
+```bash
+python tests/test_model_train.py
+```
+
+
--- tests/test_dataset_generation.py
+++ tests/test_dataset_generation.py
@@ -1,26 +1,115 @@
+import io
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import font_manager
 
+plt.switch_backend('agg')
+
 import matplotlib.patheffects as path_effects
 
-if __name__ == '__main__':
-    mpl.rcParams['font.family'] = 'sans-serif'
-    mpl.rcParams['font.sans-serif'] = ['SimHei']
-    font_manager.fontManager.addfont('./SimHei.ttf')
+mpl.rcParams['font.family'] = 'sans-serif'
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+font_manager.fontManager.addfont('./SimHei.ttf')
 
-    fig = plt.figure(figsize=(10, 10), dpi=100, facecolor='g')
-    text = fig.text(0.5, 0.5, '''
+import os
+
+os.environ["http_proxy"] = "http://127.0.0.1:8889"
+os.environ["https_proxy"] = "http://127.0.0.1:8889"
+
+from datasets import load_dataset
+
+
+def test_load_dataset():
+    dataset = load_dataset('wikitext', 'wikitext-103-raw-v1')
+    print(dataset['train']['text'][0:2])
+
+def draw_text_pil(text: str):
+    font = ImageFont.truetype('SimHei.ttf', 16)
+    img = Image.new('RGB', (500, 500), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    draw.text((10, 10), text, font=font, fill=(0, 0, 0), wrap=True)
+    return img
+
+
+def draw_text(text: str):
+    color_a_all = ['red', 'green', 'blue', 'white', 'black', 'yellow', 'purple', 'orange', 'pink']
+    color_b_all = ['red', 'green', 'blue', 'white', 'black', 'yellow', 'purple', 'orange', 'pink']
+    color_a = np.random.randint(0, len(color_a_all))
+    color_b = np.random.randint(0, len(color_b_all))
+    text_size = (550000 / len(text)) ** 0.5
+    while color_b == color_a:
+        color_b = np.random.randint(0, 2)
+    fig = plt.figure(figsize=(10, 10), dpi=100, facecolor=color_a_all[color_a])
+    text = fig.text(0.5, 0.5, text,
+                    ha='center', va='center', size=text_size, wrap=True, 
+                    color=color_b_all[color_b])
+    text.set_path_effects([path_effects.Normal()])
+    buffer = io.BytesIO()
+    canvas = fig.canvas
+    canvas.print_figure(buffer, format='png', dpi=100)
+    img = Image.open(buffer)
+    plt.close('all')
+    return img
+
+def preprocess_text(text: str):
+    text = text.replace('\n', ' ')
+    text = text.replace('\r', ' ')
+    text = ' '.join(text)
+    text = ' '.join(text.split())
+    return text
+
+def test_draw_text():
+    img = draw_text_pil(
+        text=preprocess_text(
+            '''
 施氏食狮史
 赵元任
                     
 石室诗士施氏，嗜狮，誓食十狮。施氏时时适市视狮。
 十时，适十狮适市。是时，适施氏适市。
-施氏视是十狮，恃矢势，使是十狮逝世。
-氏拾是十狮尸，适石室。石室湿，氏使侍拭石室。
-石室拭，氏始试食是十狮尸。
-食时，始识是十狮尸，实十石狮尸。试释是事。 
-                    '''.strip(),
-                    ha='center', va='center', size=60, wrap=True, color='yellow')
-    text.set_path_effects([path_effects.Normal()])
+施氏视是十狮，恃矢势，欲食之。
+十狮惧，施氏退，适市视之。
+是时，适施氏适市。
+施氏视是十狮，恃矢势，欲食之。
+十狮惧，施氏退，适市视之。
+和关于风流反馈与法律健康辅导苦于施氏。施氏食狮史，赵元任，石室诗士，嗜狮，誓食十狮。施氏时时适市视狮。十时，适十狮适市。是时，适施氏适市。施氏视是十狮，恃矢势，欲食之。十狮惧，施氏退，适市视之。是时，适施氏适市。施氏视是十狮，恃矢势，欲食之。十狮惧，施氏退，适市视之。和关于风流反馈与法律健康辅导苦于施氏。施氏食狮史，赵元任，石室诗士，嗜狮，誓食
+和vjhfgjh和关于风流反馈与法律健康辅导苦于施氏。施氏食狮史，赵元任，石室诗士，嗜狮，誓食十狮。施氏时时适市视狮。十时，适十狮适市。是时，适施氏适市。施氏视是十狮，恃矢势，欲食之。十狮惧，施氏退，适市视之。是时，适施氏适市。施氏视是十狮，恃矢势，欲食之。十狮惧，施氏退，适市视之。和关于风流反馈与法律健康辅导苦于施氏。施氏食狮史，赵
+            ''' * 2
+        )
+    )
+    img = np.asanyarray(img)
+    plt.figimage(img)
     plt.show()
+
+
+def setup_dataset():
+    from tqdm import tqdm
+    from datasets import Dataset
+    dataset = load_dataset('wikitext', 'wikitext-103-raw-v1')
+    dd = []
+    for field in dataset.keys():
+        for data in tqdm(dataset[field]['text']):
+            try:
+                data = data.strip()
+                if len(data) < 2:
+                    continue
+                text = preprocess_text(data)
+                img = draw_text(text)
+                dd.append({
+                    'text': text,
+                    'image': img
+                })
+            except:
+                continue
+    dataset = Dataset.from_list(data)
+    print(dataset)
+    dataset.save_to_disk('data')
+
+
+if __name__ == '__main__':
+    # test_draw_text()
+    # test_load_dataset()
+    setup_dataset()
+
